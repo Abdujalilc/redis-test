986f6c90dec7a8752c9e5d59090c09f82992b30b
abdujalilc <abdujalilc@outlook.com>
2023-07-19 17:25:13 +0500

    redis cookie store worked

--- RedisCookieCache/Identity/Program.cs
+++ RedisCookieCache/Identity/Program.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.Extensions.Caching.Distributed;
 using Microsoft.Extensions.Caching.StackExchangeRedis;
+using Microsoft.Extensions.Options;
+using RedisCookieCache.RedisService;
 using RedisCookieCache.Services;
 using System.Security.Claims;
 
@@ -10,17 +12,22 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddStackExchangeRedisCache(options =>
 {
     options.Configuration = "127.0.0.1:6379";
-    options.InstanceName = "SampleInstance";
 });
+
 builder.Services.Add(ServiceDescriptor.Singleton<IDistributedCache, RedisCache>());
 builder.Services.AddSingleton<ITicketStore, RedisCacheTicketStore>();
+builder.Services.AddSingleton<IPostConfigureOptions<CookieAuthenticationOptions>, ConfigureCookieAuthenticationOptions>();
+
+builder.Services.AddAuthentication("CookieAuthentication")
+                 .AddCookie("CookieAuthentication", config =>
+                 {
+                     config.Cookie.Name = "UserLoginCookie";
+                     config.LoginPath = "/Account/LoginMain";
+                     config.AccessDeniedPath = "/Account/UserAccessDenied";
+                 });
+
 builder.Services.AddOptions<CookieAuthenticationOptions>(CookieAuthenticationDefaults.AuthenticationScheme)
-    .Configure<ITicketStore>((options, store) =>
-    {
-        options.SessionStore = store;
-    });
-builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie(CookieAuthenticationDefaults.AuthenticationScheme);
+    .Configure<ITicketStore>((options, store) => { options.SessionStore = store; });
 
 builder.Services.AddAuthorization();
 
@@ -34,14 +41,14 @@ app.MapGet("/login", (HttpContext ctx) =>
     var check = ctx.User.Identity.IsAuthenticated;
     if (!check)
     {
-        ctx.SignInAsync(new ClaimsPrincipal(new ClaimsIdentity[]
+        ctx.SignInAsync(
+            new ClaimsPrincipal(new ClaimsIdentity[] {
+                new ClaimsIdentity(claims:new List<Claim>()
                 {
-            new ClaimsIdentity(claims:new List<Claim>()
-            {
-                new Claim(ClaimTypes.NameIdentifier,Guid.NewGuid().ToString())
-            },
-            authenticationType:CookieAuthenticationDefaults.AuthenticationScheme)
-                }));
+                    new Claim(ClaimTypes.NameIdentifier,Guid.NewGuid().ToString())
+                },
+                authenticationType:CookieAuthenticationDefaults.AuthenticationScheme)
+            }));
     }
     return "ok";
 }
